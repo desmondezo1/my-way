dbbf1d676b23499dff49858f127255e65c1d1c2a
nobleman97 <davidstone097@gmail.com>
2022-04-17 07:55:02 +0100

    create better storing and tracking

--- contracts/escrow.sol
+++ contracts/escrow.sol
@@ -1,94 +1,122 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "../node_modules/hardhat/console.sol";
+//import "../node_modules/hardhat/console.sol";
 
 contract Escrow {
-    //State Variables
     address public contractOwner;
-    address public vendor;
-    address public messenger;
-   
+    
     //Records
     mapping(address => uint256) balances;
     mapping(address => mapping(address => uint)) payingToMessenger;
     mapping(address => mapping(address => bool)) isPackageDelivered;
+    mapping(address => address) vendors;
+    mapping(address => address) messengers;
 
     //Events
     event txInfo(string _secret);
-
-
-    /*Functions
-    depositToContract
-    payMessenger
-    withdraw
-    goodReceived
-
-
-    */
-
-    /*Modifiers
-    onlyOwner
-    onlySeller
-
-    */
+    event feedbackLog(string _feedback);
 
     constructor(){
         contractOwner = msg.sender;
     }
 
-    modifier onlyVendor{
-        require(msg.sender == vendor, "Only a can put money up");
-        _;
-    }
 
-    function depositToContract(uint amountSent, address _messenger, string memory _secretHash)public onlyVendor payable {
+    function depositToContract(address _messenger, string memory _secretHash)public payable {
         require(msg.value > 0, "Cannot send O amount");
-        messenger = _messenger;
+        uint amountSent = msg.value;
+        address caller = msg.sender;
+
+        //store the address of the caller to prevent alterations when many persons are accessing the contract simultaneously
+        vendors[caller] = msg.sender;
+        address localMan;
+        messengers[localMan] = _messenger;
+        address takeControl = messengers[localMan];
 
         //if deposit to contract is successful, then take records
-        balances[vendor] += amountSent;
-        payingToMessenger[vendor][messenger] += amountSent;
+        balances[caller] += amountSent;
+        payingToMessenger[caller][takeControl] += amountSent;
 
         //...then say package has not been delivered
-        isPackageDelivered[vendor][messenger] = false;
+        isPackageDelivered[caller][takeControl] = false;
         emit txInfo(_secretHash);
     }
 
-    function confirmDelivery(bool _isConfirmed) public{
-
-        isPackageDelivered[vendor][messenger] = _isConfirmed;
-
-        if(isPackageDelivered[vendor][messenger]){
-            payMessenger();
-        }else{
-            //report that delivery has not been confirmed
-        }
+    function getStagedMoney(address vendor, address messenger) public view returns(uint){
+        return payingToMessenger[vendor][messenger];
     }
 
-    function payMessenger()internal{
+    function confirmDelivery(bool _isConfirmed, address _vendor, address _messenger) public {
+
+        isPackageDelivered[_vendor][_messenger] = _isConfirmed;
+
+        //set conditions for payment
+        if(isPackageDelivered[_vendor][_messenger]){
+
+            //how much did seller and messenger agree on?
+            uint agreedPrice = payingToMessenger[_vendor][_messenger];
+
+            //remove that amount from the records
+            balances[_vendor] -= agreedPrice;
+            payingToMessenger[_vendor][_messenger] -= agreedPrice;
+
+            //then try paying the messenger. If it fails, re-update the records
+            if(payable(_messenger).send(agreedPrice)){
+                emit feedbackLog("messenger paid successfully");
+            }else{
+                balances[_vendor] += agreedPrice;
+                payingToMessenger[_vendor][_messenger] += agreedPrice;
+                emit feedbackLog("could not pay messenger. Money returned successfully");
+            }
+        }else{
+            emit feedbackLog("Transaction not completed");
+        }
+
+       /* ADD THIS LOGIC TO web3.js
+       
+        if(isPackageDelivered[_vendor][_messenger]){
+            payMessenger(_vendor, _messenger);
+        }else{
+            emit feedbackLog("Sorry! Your transaction has not been verified");
+        }*/
+    }
+
+    function payMessenger(address _vendor, address messenger)internal{
         //set conditions for payment
 
         //how much did seller and messenger agree on?
-        uint agreedPrice = payingToMessenger[vendor][messenger];
+        uint agreedPrice = payingToMessenger[_vendor][messenger];
 
         //remove that amount from the records
-        balances[vendor] -= agreedPrice;
-        payingToMessenger[vendor][messenger] -= agreedPrice;
+        balances[_vendor] -= agreedPrice;
+        payingToMessenger[_vendor][messenger] -= agreedPrice;
 
         //then try paying the messenger. If it fails, re-update the records
         if(payable(messenger).send(agreedPrice)){
-            console.log("messenger paid successfully");
+            emit feedbackLog("messenger paid successfully");
         }else{
-            balances[vendor] += agreedPrice;
-            payingToMessenger[vendor][messenger] += agreedPrice;
-            console.log("could not pay messenger. Money returned successfully");
+            balances[_vendor] += agreedPrice;
+            payingToMessenger[_vendor][messenger] += agreedPrice;
+            emit feedbackLog("could not pay messenger. Money returned successfully");
         }
     }
 
-    function sellerWithdraw()public {
-        
+    
+    function returnMoneyToVendor(address __vendor, address _messenger) public {
+        require(msg.sender == contractOwner, "Only Admin can do this");
+        uint amount = payingToMessenger[__vendor][_messenger];
 
+        balances[__vendor] -= amount;
+        payingToMessenger[__vendor][_messenger] = 0;
+
+
+        if(payable(__vendor).send(amount)){
+            emit feedbackLog("withdrawal successful");
+        }else{
+            
+            balances[__vendor] += amount;
+            payingToMessenger[__vendor][_messenger] += amount;
+            emit feedbackLog("withdrawal failed. Money returned");
+        }
     }
-
 }
