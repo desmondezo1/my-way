db2d74d3fa51f7fde65f70ad8917126bf1f6887a
JoshX [MacBookPro] <joshua.osagie01@gmail.com>
2022-04-18 11:52:36 +0100

    feat: commuters interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
         secondary: '#6B7A6B',
         info: '#0E670E',
         'info-light': '#D8F3DB',
-        main: '#92DD99'
+        main: '#92DD99',
+        shadow: '#8A9E8A'
       })
     }
   },
