d5afb7bad19b6e5acaef9de04c59cb9707a7f431
Desmond <desezo@gmail.com>
2022-05-08 15:57:45 +0100

    initialized firebase

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import  { getFirestore, collection } from firbase/getFirestore;
+require('dotenv').config()
  
 const firebaseConfig = {
-  apiKey: "AIzaSyAjFiHspHycMyexeiIJX7srwrvQsdx048w",
-  authDomain: "my-way-c4beb.firebaseapp.com",
-  projectId: "my-way-c4beb",
-  storageBucket: "my-way-c4beb.appspot.com",
-  messagingSenderId: "606398663837",
-  appId: "1:606398663837:web:a6901fa2fa5880bcd0d874",
-  measurementId: "G-8JP23TF0F6"
+  apiKey: process.env('FIREBASE_APIKEY'),
+  authDomain: process.env('FIREBASE_AUTHDOMAIN'),
+  projectId: process.env('FIREBASE_PROJECTID'),
+  storageBucket: process.env('FIREBASE_STORAGE_BUCKET'),
+  messagingSenderId: process.env('FIREBASE_MESSAGING_SENDERID'),
+  appId: process.env('FIREBASE_APPID'),
+  measurementId: process.env('FIREBASE_MEASUREMENTID')
 };
 
 // Initialize Firebase
